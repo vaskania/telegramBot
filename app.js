3fbcdd335496f2914aba7d5129d2f1604d4176e8
vaskaHome <vaskania26@gmail.com>
2021-12-04 12:00:33 +0400

    added telegram bot with dockerfile

--- app.js
+++ app.js
@@ -3,13 +3,43 @@ require('dotenv').config();
 const TOKEN = process.env.TELEGRAM_TOKEN || 'YOUR_TELEGRAM_BOT_TOKEN';
 const TelegramBot = require('node-telegram-bot-api');
 
-const options = {
+const bot = new TelegramBot(TOKEN, {
   polling: true,
-};
-const bot = new TelegramBot(TOKEN, options);
+});
 
-// Matches /echo [whatever]
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  const resp = match[1];
-  bot.sendMessage(msg.chat.id, resp);
+const links = 'https://git.foxminded.com.ua/foxstudent100710\nhttps://github.com/vaskania';
+
+bot.on('message', (msg) => {
+  const text = msg.text.toLocaleLowerCase().trim();
+  const chatId = msg.chat.id;
+
+  if (text === '/start' || text === '/help') {
+    return bot.sendMessage(
+      chatId,
+      `Hello ${msg.chat.username}, for more information about me please send /about or /links message.`,
+      {
+        reply_markup: {
+          keyboard: [['/about', '/links']],
+          resize_keyboard: true,
+          one_time_keyboard: true,
+        },
+      },
+    );
+  }
+
+  if (text === '/about') {
+    return bot.sendMessage(chatId, 'My name is Vaskania');
+  }
+
+  if (text === '/links') {
+    return bot.sendMessage(chatId, links, { parse_mode: 'HTML' });
+  }
+
+  return bot.sendMessage(chatId, 'Please send available commands', {
+    reply_markup: {
+      keyboard: [['/about', '/links']],
+      resize_keyboard: true,
+      one_time_keyboard: true,
+    },
+  });
 });
